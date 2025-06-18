25ce8c44ceebd09b993f4162b62cbb5e007b727f
YasukiNakamura <nya.s.k0214@gmail.com>
2025-06-18 15:49:24 +0900

    complete

--- ex7.py
+++ ex7.py
@@ -6,7 +6,7 @@ model = YOLO('othello.pt')
 
 # 画像読み込み
 image_path = 'ex4.jpg'
-conf_threshold = 0.99  # 盤外の誤検出を防ぐため信頼度を上げる（例: 0.99）
+conf_threshold = 0.99  # 盤外の誤検出を防ぐため
 
 # 推論実行
 results = model(image_path, conf=conf_threshold)
@@ -21,13 +21,12 @@ for box in detections.boxes:
     cls_id = int(box.cls)
     conf = float(box.conf)
     if conf < conf_threshold:
-        continue  # 信頼度が低い検出は無視
+        continue  
 
     if cls_id == 0:
         white_count += 1
     elif cls_id == 1:
         black_count += 1
 
-# 結果出力
 print(f"白石の数: {white_count}")
 print(f"黒石の数: {black_count}")
